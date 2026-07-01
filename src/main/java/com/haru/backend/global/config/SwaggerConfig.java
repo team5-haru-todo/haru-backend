8f0ea6d14ac6a785b8abe888ed3b9af9fd1a3936
heewon-choi <chw9285@gmail.com>
2026-07-01 09:18:12 +0900

    docs(record): document authenticated record APIs

--- src/main/java/com/haru/backend/global/config/SwaggerConfig.java
+++ src/main/java/com/haru/backend/global/config/SwaggerConfig.java
@@ -1,8 +1,10 @@
 package com.haru.backend.global.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,6 +19,12 @@ public class SwaggerConfig {
                         .description("하루한개 서비스 백엔드 API 문서")
                         .version("v1.0.0")
                         .license(new License()
-                                .name("Haru Project")));
+                                .name("Haru Project")))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
     }
 }
